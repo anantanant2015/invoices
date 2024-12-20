bc251c67c12e32054858d7abdfcd29aca95f2a7b
Anant <anantanant2015@gmail.com>
2024-12-21 04:05:48 +0530

    Updates offer and product struct

--- lib/offer.ex
+++ lib/offer.ex
@@ -1,24 +1,3 @@
 defmodule Invoices.Offer do
   defstruct [:type, :target, :value, :discount_price]
-
-  @offers []
-
-  # Macro to define an offer
-  defmacro add_new_offer(type, target, value, discount_price) do
-    quote do
-      @offers [
-        %Offer{
-          type: unquote(type),
-          target: unquote(target),
-          value: unquote(value),
-          discount_price: unquote(discount_price)
-        }
-        | @offers
-      ]
-    end
-  end
-
-  def list_offers do
-    @offers
-  end
 end
