13421396b54dae6718c6dd6b82c8da9efb6abfd6
Anant <anantanant2015@gmail.com>
2024-12-21 04:05:48 +0530

    Updates offer and product struct

--- lib/product.ex
+++ lib/product.ex
@@ -1,18 +1,3 @@
 defmodule Invoices.Product do
   defstruct [:name, :code, :price]
-
-  @products []
-
-  # Macro to define a product
-  defmacro add_new_product(name, code, price) do
-    quote do
-      @products [
-        %Product{name: unquote(name), code: unquote(code), price: unquote(price)} | @products
-      ]
-    end
-  end
-
-  def list_products do
-    @products
-  end
 end
