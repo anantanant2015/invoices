fbe58d97dcf2ccf427cdccd4b8b70ffd403f8108
Anant <anantanant2015@gmail.com>
2024-12-21 04:07:57 +0530

    Adds supervisor

---
+++ lib/supervisor.ex
@@ -0,0 +1,16 @@
+defmodule Invoices.Supervisor do
+  use Supervisor
+
+  def start_link(init_arg \\ []) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+    children = [
+      {Invoices.Server, %{refs: %{}, products: [], offers: []}}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
